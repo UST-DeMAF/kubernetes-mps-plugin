876d97744d8d192ac340443b6f33b8ce13e9f175
Marcel Weller <marcefrederik@aol.de>
2023-04-20 16:16:07 +0200

    integrated latest version of mps transformer

---
+++ src/test/java/ust/tad/kubernetesmpsplugin/analysis/TransformationServiceTest.java
@@ -0,0 +1,54 @@
+package ust.tad.kubernetesmpsplugin.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ust.tad.kubernetesmpsplugin.kubernetesmodel.KubernetesDeploymentModel;
+import ust.tad.kubernetesmpsplugin.kubernetesmodel.deployment.*;
+import ust.tad.kubernetesmpsplugin.kubernetesmodel.service.KubernetesService;
+import ust.tad.kubernetesmpsplugin.kubernetesmodel.service.Selector;
+import ust.tad.kubernetesmpsplugin.kubernetesmodel.service.ServicePort;
+import ust.tad.kubernetesmpsplugin.models.tadm.TechnologyAgnosticDeploymentModel;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class TransformationServiceTest {
+
+    @Autowired
+    TransformationService transformationService;
+
+    @Test
+    public void runTransformationSuccessful() throws IOException {
+        KubernetesDeploymentModel inputModel = createDummyModel();
+        TechnologyAgnosticDeploymentModel result = transformationService.transformInternalToTADM(new TechnologyAgnosticDeploymentModel(), inputModel);
+        assertNotNull(result);
+        System.out.println(result);
+    }
+
+
+    private KubernetesDeploymentModel createDummyModel() {
+        Label label = new Label("app", "dummyApp");
+        ContainerPort containerPort = new ContainerPort("containerPort", 8080);
+        EnvironmentVariable environmentVariable = new EnvironmentVariable("dummyEnv", "dummyEnvVal");
+        Container dummyContainer = new Container("dummyContainer",
+                "dummyRepo/dummy:main", Set.of(containerPort), Set.of(environmentVariable));
+        KubernetesDeployment dummyDeployment = new KubernetesDeployment("dummyDeployment",
+                3, Set.of(label), Set.of(dummyContainer));
+        KubernetesDeployment dummyDeploymentTwo = new KubernetesDeployment("dummyDeploymentTwo",
+                1, Set.of(label), Set.of(dummyContainer));
+
+        ServicePort servicePort = new ServicePort("defaultPort", 80, "8080");
+        Selector selector = new Selector("app", "dummyApp");
+        KubernetesService dummyService = new KubernetesService("dummyService",
+                Set.of(servicePort), Set.of(selector));
+
+        return new KubernetesDeploymentModel(
+                Set.of(dummyDeployment, dummyDeploymentTwo),
+                Set.of(dummyService));
+    }
+
+}
