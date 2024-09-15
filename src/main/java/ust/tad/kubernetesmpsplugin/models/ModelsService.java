af9831ef0508d5065bff151b328368bd3feea7ec
365Bit <20995752+365Bit@users.noreply.github.com>
2024-09-15 21:10:41 +0200

    Linting

--- src/main/java/ust/tad/kubernetesmpsplugin/models/ModelsService.java
+++ src/main/java/ust/tad/kubernetesmpsplugin/models/ModelsService.java
@@ -1,7 +1,6 @@
 package ust.tad.kubernetesmpsplugin.models;
 
 import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,86 +8,93 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import ust.tad.kubernetesmpsplugin.models.tadm.TechnologyAgnosticDeploymentModel;
 import ust.tad.kubernetesmpsplugin.models.tsdm.TechnologySpecificDeploymentModel;
 
 @Service
 public class ModelsService {
 
-    private static final Logger LOG =
-      LoggerFactory.getLogger(ModelsService.class);
-    
-    @Autowired
-    private WebClient modelsServiceApiClient;
+  private static final Logger LOG = LoggerFactory.getLogger(ModelsService.class);
 
-    /**
-     * Retrieve a technology-specific deployment model from the model service.
-     * 
-     * @param transformationProcessId
-     * @return
-     */
-    public TechnologySpecificDeploymentModel getTechnologySpecificDeploymentModel(UUID transformationProcessId) {
-        LOG.info("Requesting technology-specific deployment model");
-        return modelsServiceApiClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/technology-specific/"+transformationProcessId)
-                .build())
-           .accept(MediaType.APPLICATION_JSON)
-           .retrieve()
-           .bodyToMono(TechnologySpecificDeploymentModel.class)
-           .block();
-    }
+  @Autowired private WebClient modelsServiceApiClient;
 
-    /**
-     * Update a technology-specific deployment model by sending it to the update endpoint of the models service.
-     * 
-     * @param technologySpecificDeploymentModel
-     */
-    public void updateTechnologySpecificDeploymentModel(TechnologySpecificDeploymentModel technologySpecificDeploymentModel) {
-        LOG.info("Updating technology-specific deployment model");
-        modelsServiceApiClient.post()
-            .uri("/technology-specific")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .body(BodyInserters.fromValue(technologySpecificDeploymentModel))
-            .retrieve()
-            .bodyToMono(TechnologySpecificDeploymentModel.class)
-            .block();
-    }
+  /**
+   * Retrieve a technology-specific deployment model from the model service.
+   *
+   * @param transformationProcessId
+   * @return
+   */
+  public TechnologySpecificDeploymentModel getTechnologySpecificDeploymentModel(
+      UUID transformationProcessId) {
+    LOG.info("Requesting technology-specific deployment model");
+    return modelsServiceApiClient
+        .get()
+        .uri(
+            uriBuilder ->
+                uriBuilder.path("/technology-specific/" + transformationProcessId).build())
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(TechnologySpecificDeploymentModel.class)
+        .block();
+  }
 
-    /**
-     * Retrieve a technology-agnostic deployment model from the model service.
-     * 
-     * @param transformationProcessId
-     * @return
-     */
-    public TechnologyAgnosticDeploymentModel getTechnologyAgnosticDeploymentModel(UUID transformationProcessId) {
-        LOG.info("Requesting technology-agnostic deployment model");
-         return modelsServiceApiClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/technology-agnostic/"+transformationProcessId)
-                .build())
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .bodyToMono(TechnologyAgnosticDeploymentModel.class)
-            .block();
-    }
+  /**
+   * Update a technology-specific deployment model by sending it to the update endpoint of the
+   * models service.
+   *
+   * @param technologySpecificDeploymentModel
+   */
+  public void updateTechnologySpecificDeploymentModel(
+      TechnologySpecificDeploymentModel technologySpecificDeploymentModel) {
+    LOG.info("Updating technology-specific deployment model");
+    modelsServiceApiClient
+        .post()
+        .uri("/technology-specific")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(technologySpecificDeploymentModel))
+        .retrieve()
+        .bodyToMono(TechnologySpecificDeploymentModel.class)
+        .block();
+  }
 
-    /**
-     * Update a technology-agnostic deployment model by sending it to the update endpoint of the models service.
-     * 
-     * @param technologyAgnosticDeploymentModel
-     */
-    public void updateTechnologyAgnosticDeploymentModel(TechnologyAgnosticDeploymentModel technologyAgnosticDeploymentModel) {
-        LOG.info("Updating technology-agnostic deployment model");
-        modelsServiceApiClient.post()
-            .uri("/technology-agnostic")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .body(BodyInserters.fromValue(technologyAgnosticDeploymentModel))
-            .retrieve()
-            .bodyToMono(TechnologyAgnosticDeploymentModel.class)
-            .block();
-    }
+  /**
+   * Retrieve a technology-agnostic deployment model from the model service.
+   *
+   * @param transformationProcessId
+   * @return
+   */
+  public TechnologyAgnosticDeploymentModel getTechnologyAgnosticDeploymentModel(
+      UUID transformationProcessId) {
+    LOG.info("Requesting technology-agnostic deployment model");
+    return modelsServiceApiClient
+        .get()
+        .uri(
+            uriBuilder ->
+                uriBuilder.path("/technology-agnostic/" + transformationProcessId).build())
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(TechnologyAgnosticDeploymentModel.class)
+        .block();
+  }
+
+  /**
+   * Update a technology-agnostic deployment model by sending it to the update endpoint of the
+   * models service.
+   *
+   * @param technologyAgnosticDeploymentModel
+   */
+  public void updateTechnologyAgnosticDeploymentModel(
+      TechnologyAgnosticDeploymentModel technologyAgnosticDeploymentModel) {
+    LOG.info("Updating technology-agnostic deployment model");
+    modelsServiceApiClient
+        .post()
+        .uri("/technology-agnostic")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(technologyAgnosticDeploymentModel))
+        .retrieve()
+        .bodyToMono(TechnologyAgnosticDeploymentModel.class)
+        .block();
+  }
 }
